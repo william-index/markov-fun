0aa863321e9d40954af79d5a3719abee66ba0f4e
William Anderson <thewilliamanderson@gmail.com>
2017-03-12 13:20:44 -0400

    Parses text and trains basic non-percentized model

--- Trainer/DataTrainer.py
+++ Trainer/DataTrainer.py
@@ -1,23 +1,92 @@
-import attr
+import attr, re
 
 @attr.s
 class DataTrainer(object):
-    foo = attr.ib()
-
 
     """
-    Sample method
+    Trains a markov model of a given order off a given text file.
+
+    Args:
+        filename (string) : path to file from project root
+        order (int) : depth of words/tokens to construct keys from
+    Returns:
+        Dict: trained markov model
     """
-    def sample(self, bat):
-        print("Called sample method")
-        myfile = open('data/training/william-blog-training-raw.txt', 'r')
-        data = myfile.read()
-        myfile.close()
+    def train_text_data(self, filename, order):
+        raw_data = self.get_raw_file_data(filename)
+        cleaned_text = self.strip_special_chars(raw_data)
+        data = self.crawl_clean_text(text=cleaned_text, order=order)
+        probabilities = self.probabilitize_data(data)
 
-        newfile = open('data/processed/sample.txt', 'w')
-        newfile.write(data)
-        newfile.close()
+        return probabilities
 
-        print(data)
 
-        return self.foo + bat
+    def probabilitize_data(self, data):
+        # @TODO pase lists for keys into dicts of keys and percentages
+        return data
+
+    """
+    Reads a text file and returns its raw data.
+
+    Args:
+        filename (string) : path to file from project root
+    Returns:
+        string: contents of file
+    """
+    def get_raw_file_data(self, filename):
+        raw_file = open(filename, 'r')
+        raw_data = raw_file.read()
+        raw_file.close()
+
+        return raw_data
+
+    """
+    Strips out special characters except select punctuation and indicates EOL in
+    place of said select punctuation.
+
+    Args:
+        data (string) : raw text data
+    Returns:
+        string: alpha-numeric and spaces only string with ".", "!", and "?"
+                replaced by "EOL"
+    """
+    def strip_special_chars(self, data):
+        cleaned_text = re.sub('!|\.|\?', ' EOL ', data)
+        cleaned_text = re.sub('[^A-Za-z0-9\s]+', ' ', cleaned_text)
+        cleaned_text = re.sub( '\s+', ' ', cleaned_text).strip()
+        return cleaned_text
+
+    """
+    Splits a string by spaces and creates a dict of lists for all possibilities
+    of N order.
+
+    Args:
+        test (string) : string of text
+        order (int) : depth of words/tokens to construct keys from
+    Returns:
+        Dict: Dict of lists for all n Order keys and all possibilities
+    """
+    def crawl_clean_text(self, text, order):
+        text_seq = text.split()
+        tree = {}
+
+        for i in range(0, len(text_seq) - order):
+            key = ""
+            for j in range(0, order):
+                key = key + " " + text_seq[i+j]
+            key = key[1:]
+
+            if not key in tree:
+                tree[key] = []
+            tree[key].append(text_seq[i + order])
+
+        return tree
+
+    """
+    @TODO saves trained data to file in re-readable format
+    """
+    def save_data_to_file(self):
+        # newfile = open('data/processed/sample.txt', 'w')
+        # newfile.write(data)
+        # newfile.close()
+        pass
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from Trainer import DataTrainer
 
-trainer = DataTrainer(
-    foo="what"
-    )
+trainer = DataTrainer()
 
-x = trainer.sample(bat="cat")
+x = trainer.train_text_data(
+        filename = "data/training/william-blog-training-raw.txt",
+        order = 1
+        )
+
 print(x)
