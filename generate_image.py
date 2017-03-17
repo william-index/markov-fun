791e42c6c96543ad9eca55eafa63fff199019fb5
William <wanderson@hugeinc.com>
2017-03-17 15:32:03 -0400

    simplified training for images

--- generate_image.py
+++ generate_image.py
@@ -7,9 +7,13 @@ trainer = DataTrainer()
 stepper = Stepper()
 img_sequencer = ImageSequencer()
 
-bauhaus_images = ['bauhaus1.png', 'bauhaus2.png', 'bauhaus3.png', 'bauhaus4.png']
+image_set = []
+phrase = "dia_de_los_meurtos"
+for i in range(0, 20):
+    image_set.append('{phrase}/{phrase}-{i}.jpg'.format(phrase=phrase, i=i))
+
 concat_text = ""
-for image in bauhaus_images:
+for image in image_set:
     image = Image.open("data/training/images/cropped_and_reduced/{image}".format(image=image))
     image_as_text = img_sequencer.sequence_image_to_text(image)
     concat_text = concat_text + ' ' + image_as_text
@@ -19,15 +23,15 @@ concat_text.strip()
 
 trained_data = trainer.train_text_data(
         raw_text = concat_text,
-        order = 100,
+        order = 2,
         )
 
 gen_seq = stepper.new_set_length_sequence(
         model = trained_data,
-        steps = 200 * 200
+        steps = 100 * 100
         )
 
-image = img_sequencer.convert_text_to_image(gen_seq, 200, 200)
+image = img_sequencer.convert_text_to_image(gen_seq, 100, 100)
 
-image.save("data/processed/images/bauhaus5.png")
+image.save("data/processed/images/sample.png")
 # print(gen_seq)
