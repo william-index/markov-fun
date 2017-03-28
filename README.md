4a8f81ecae9fbe67536d09ab28104ae1717681eb
William <wanderson@hugeinc.com>
2017-03-28 13:46:35 -0400

    setsup args for image generation

--- README.md
+++ README.md
@@ -26,5 +26,18 @@ the string is the work that google will search for images for
   * for example a cRange of 2 would enable a red value of either 0 or 255 where a cRange of 4 would allow 0,85,170,255 as possible values
 
 
+#### Generating Images from Image sets
+An image can be generated from any set of images within a directory
+
+by calling
+```
+python generate_image.py '<string>' --size <int> <int> --norder <int>
+```
+
+* **<string>** - path to image directory
+* **size** - 2 ints for width and height respectively that represent the size of all images in the set
+* **norder** - what order markov chain to use
+
+
 #### Stepper
 Stepper Steps through a trained model to create a phrase from its tokens, starting with a random key.
--- generate_image.py
+++ generate_image.py
@@ -1,3 +1,5 @@
+import argparse
+import os
 from PIL import Image
 from Trainer import DataTrainer, ImageSequencer
 from Stepper import Stepper
@@ -7,14 +9,25 @@ trainer = DataTrainer()
 stepper = Stepper()
 img_sequencer = ImageSequencer()
 
+parser = argparse.ArgumentParser(description='Collect and curate a dataset of images.')
+parser.add_argument('directory', nargs=1)
+parser.add_argument('--size', nargs=2, type=int)
+parser.add_argument('--norder', nargs=1, type=int)
+
+args = parser.parse_args()
+directory = args.directory[0]
+im_width, im_height = args.size
+norder = args.norder[0]
+
+
 image_set = []
-phrase = "nightmare"
-for i in range(0, 20):
-    image_set.append('{phrase}/{phrase}-{i}.jpg'.format(phrase=phrase, i=i))
+for fn in os.listdir(directory):
+    if fn[0] != '.':
+        image_set.append(directory + fn)
 
 concat_text = ""
 for image in image_set:
-    image = Image.open("data/training/images/cropped_and_reduced/{image}".format(image=image))
+    image = Image.open(image)
     image_as_text = img_sequencer.sequence_image_to_text(image)
     concat_text = concat_text + ' ' + image_as_text
 
@@ -23,15 +36,15 @@ concat_text.strip()
 
 trained_data = trainer.train_text_data(
         raw_text = concat_text,
-        order = 30,
+        order = norder,
         )
 
 gen_seq = stepper.new_set_length_sequence(
         model = trained_data,
-        steps = 100 * 100
+        steps = im_width * im_height
         )
 
-image = img_sequencer.convert_text_to_image(gen_seq, 100, 100)
+image = img_sequencer.convert_text_to_image(gen_seq, im_width, im_height)
 
 image.save("data/processed/images/sample.png")
 # print(gen_seq)
